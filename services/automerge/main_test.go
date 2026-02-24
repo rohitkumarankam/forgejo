1dca7d4142abe94e7e1315c03248022758755cb6
Gusted <postmaster@gusted.xyz>
2026-02-24 02:04:49 +0100

    chore: unit testing

---
+++ services/automerge/main_test.go
@@ -0,0 +1,14 @@
+// Copyright 2026 The Forgejo Authors. All rights reserved.
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+package automerge
+
+import (
+	"testing"
+
+	"forgejo.org/models/unittest"
+)
+
+func TestMain(m *testing.M) {
+	unittest.MainTest(m)
+}
