733a390ecd1609a72f4123c9ab5101b85f3437e9
Mathieu Fenniak <mathieu@fenniak.net>
2026-04-29 05:26:22 +0200

    fix: verify PR author has write access to head to support allow maintainers edit (#12292)
    When a pull request is opened, the author is able to mark that pull request to "Allow edits from maintainers", which grants the maintainers of the pull request's repo access to edit the pull request branch contents.  It is possible to create a pull request where the pull request author does not have the ability to edit the pull request branch.  Due to a missing security check for this case, maintainers of the pull request repo would be granted the ability to edit the pull request branch, even if the author of the pull request did not have that ability.  By exploiting this missing security check, a user can edit any branch in a repository if they're able to fork that repository.  The issue is being fixed by restricting the scope of "Allow edits from maintainers" to only grant that access if the pull request author also had access to edit the branch.
    
    Thanks to Arvin Shivram of Brutecat Security for discovering and responsibly disclosing the vulnerability.
    
    Reviewed-on: https://codeberg.org/forgejo/forgejo/pulls/12292
    Reviewed-by: 0ko <0ko@noreply.codeberg.org>

--- services/pull/pull.go
+++ services/pull/pull.go
@@ -534,7 +534,7 @@ func CloseBranchPulls(ctx context.Context, doer *user_model.User, repoID int64, 
 	}
 
 	prs = append(prs, prs2...)
-	if err := issues_model.PullRequestList(prs).LoadAttributes(ctx); err != nil {
+	if err := prs.LoadAttributes(ctx); err != nil {
 		return err
 	}
 
@@ -564,7 +564,7 @@ func CloseRepoBranchesPulls(ctx context.Context, doer *user_model.User, repo *re
 			return err
 		}
 
-		if err = issues_model.PullRequestList(prs).LoadAttributes(ctx); err != nil {
+		if err = prs.LoadAttributes(ctx); err != nil {
 			return err
 		}
 
